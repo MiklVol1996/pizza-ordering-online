6f3fba2162d90b51fbfcb0fc0c1cbf9c2d9bdcd2
MiklVol <pianomihail10@mail.ru>
2023-08-13 10:52:32 +0300

    Updates

---
+++ static/js/357.cb0d5d20.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpizzaz=self.webpackChunkpizzaz||[]).push([[357],{7357:function(n,s,e){e.r(s),e.d(s,{default:function(){return a}});e(2791);var r="notFound_wrap__+DKKF",u=e(184),a=function(){return(0,u.jsx)("div",{className:r,children:(0,u.jsxs)("h1",{children:[(0,u.jsx)("span",{children:"\ud83d\ude15"}),(0,u.jsx)("br",{}),"\u0422\u0430\u043a\u043e\u0439 \u0441\u0442\u0440\u0430\u043d\u0438\u0446\u044b \u043d\u0435\u0442"]})})}}}]);
+//# sourceMappingURL=357.cb0d5d20.chunk.js.map
